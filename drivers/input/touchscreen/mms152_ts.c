ca7e0634aa22515c62523539ddafc5a01de01467
rogersb11 <brettrogers11@gmail.com>
2014-10-10 17:43:15 -0400

    Revert "mms153_ts: fix build error"
    This reverts commit a2eb1635998cae5afe3131f73e087b7b9185eea8.

--- drivers/input/touchscreen/mms152_ts.c
+++ drivers/input/touchscreen/mms152_ts.c
@@ -1310,7 +1310,7 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 #endif
 
 					// report state to cypress-touchkey for backlight timeout
-//ff					touchscreen_state_report(0);
+					touchscreen_state_report(0);
 					
 #ifdef CONFIG_LCD_FREQ_SWITCH
 					dev_notice(&client->dev,
@@ -1355,7 +1355,7 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 #endif
 
 					// report state to cypress-touchkey for backlight timeout
-//ff					touchscreen_state_report(0);
+					touchscreen_state_report(0);
 
 #ifdef CONFIG_LCD_FREQ_SWITCH
 					//dev_notice(&client->dev,
@@ -1400,7 +1400,7 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 #endif
 
 					// report state to cypress-touchkey for backlight timeout
-//ff					touchscreen_state_report(0);
+					touchscreen_state_report(0);
 
 					dev_notice(&client->dev,
 						"R [%2d],([%4d],[%3d]),S:%d W:%d",
