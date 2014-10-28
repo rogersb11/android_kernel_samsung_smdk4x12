b3289a44320c46df45df9f6f3a71f6f97276c3ff
rogersb11 <brettrogers11@gmail.com>
2014-10-27 23:33:30 -0400

    Fix vibration control, add missing code and remove sammy call from 8997(not shown)thanks to @gokhanmoral

--- drivers/motor/max8997_vibrator.c
+++ drivers/motor/max8997_vibrator.c
@@ -1,3 +1,16 @@
+/*
+ * haptic motor driver for max8997 - max8997_vibrator.c
+ *
+ * Copyright (C) 2011 Unknown Samsung Employees (Original file was missing copyright header)
+ * Copyright (C) 2012 The CyanogenMod Project
+ *                    Daniel Hillenbrand <codeworkx@cyanogenmod.com>
+ *                    Andrew Dodd <atd7@cornell.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/timed_output.h>
@@ -14,6 +27,12 @@
 #include "mach/gpio.h"
 #endif
 
+static int pwm_duty_max;
+static int pwm_duty_min;
+
+unsigned long pwm_value = 100; /* duty in percent */
+static int pwm_duty; /* duty value */
+
 struct vibrator_drvdata {
 	struct max8997_motor_data *pdata;
 	struct pwm_device	*pwm;
@@ -27,12 +46,8 @@ struct vibrator_drvdata {
 	int timeout;
 };
 
-static const int vibrator_duty_levels[] = { 26000, 28000, 30000, 32000, 34000, 36000, 38000, 40000, 42000, 44000 };
-static int vibrator_level = -1;
-static int vibrator_duty = -1;
-
 #ifdef CONFIG_VIBETONZ
-struct vibrator_drvdata *g_data;
+static struct vibrator_drvdata *g_data;
 #endif
 
 static int vibetonz_clk_on(struct device *dev, bool en)
@@ -93,7 +108,7 @@ static void vibrator_work(struct work_struct *_work)
 	struct vibrator_drvdata *data =
 		container_of(_work, struct vibrator_drvdata, work);
 
-	printk(KERN_DEBUG "[VIB] time = %dms\n", data->timeout);
+	pr_debug("[VIB] time = %dms\n", data->timeout);
 
 	if (0 == data->timeout) {
 		if (!data->running)
@@ -116,13 +131,8 @@ static void vibrator_work(struct work_struct *_work)
 		i2c_max8997_hapticmotor(data, true);
 		pwm_config(data->pwm,
 			data->pdata->duty, data->pdata->period);
-} else {
-		if (vibrator_duty < 0) {
-			pwm_config(data->pwm, pwm_duty, data->pdata->period);
-			pr_debug("[VIB] %s: pwm_config duty=%d\n", __func__, pwm_duty);
-		} else
-			pwm_config(data->pwm, vibrator_duty, 44138);
-}
+		pwm_config(data->pwm, pwm_duty, data->pdata->period);
+		pr_debug("[VIB] %s: pwm_config duty=%d\n", __func__, pwm_duty);
 		pwm_enable(data->pwm);
 
 		data->running = true;
@@ -200,7 +210,7 @@ void vibtonz_pwm(int nForce)
 	/* add to avoid the glitch issue */
 	static int prev_duty;
 	int pwm_period = data->pdata->period;
-	int pwm_duty = pwm_period/2 + ((pwm_period/2 - 2) * nForce)/127;
+	pwm_duty = pwm_period/2 + ((pwm_period/2 - 2) * nForce)/127;
 
 #if defined(CONFIG_MACH_P4)
 	if (pwm_duty > data->pdata->duty)
@@ -212,64 +222,53 @@ void vibtonz_pwm(int nForce)
 	/* add to avoid the glitch issue */
 	if (prev_duty != pwm_duty) {
 		prev_duty = pwm_duty;
+		pr_debug("[VIB] %s: setting pwm_duty=%d", __func__, pwm_duty);
 		pwm_config(data->pwm, pwm_duty, pwm_period);
 	}
 }
 EXPORT_SYMBOL(vibtonz_pwm);
+
+static ssize_t pwm_value_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	int count;
+
+	pwm_value = ((pwm_duty - pwm_duty_min) * 100) / pwm_duty_min;
+
+	count = sprintf(buf, "%lu\n", pwm_value);
+	pr_debug("[VIB] pwm_value: %lu\n", pwm_value);
+
+	return count;
+}
+
+ssize_t pwm_value_store(struct device *dev,
+		struct device_attribute *attr,
+		const char *buf, size_t size)
+{
+	if (kstrtoul(buf, 0, &pwm_value))
+		pr_err("[VIB] %s: error on storing pwm_value\n", __func__); 
+
+	pr_info("[VIB] %s: pwm_value=%lu\n", __func__, pwm_value);
+
+	pwm_duty = (pwm_value * pwm_duty_min) / 100 + pwm_duty_min;
+
+	/* make sure new pwm duty is in range */
+	if (pwm_duty > pwm_duty_max) {
+		pwm_duty = pwm_duty_max;
+	}
+	else if (pwm_duty < pwm_duty_min) {
+		pwm_duty = pwm_duty_min;
+	}
+
+	pr_info("[VIB] %s: pwm_duty=%d\n", __func__, pwm_duty);
+
+	return size;
+}
+static DEVICE_ATTR(pwm_value, S_IRUGO | S_IWUSR,
+		pwm_value_show, pwm_value_store);
 #endif
 
-static ssize_t show_vibrator_level_max(struct device *dev,
-				       struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%d\n", (ARRAY_SIZE(vibrator_duty_levels) - 1));
-}
-
-static ssize_t show_vibrator_level(struct device *dev,
-				   struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "vibrator level: %d, duty: %d\n", vibrator_level, vibrator_duty);
-}
-
-static ssize_t store_vibrator_level(struct device *dev,
-				    struct device_attribute *attr,
-				    const char *buf, size_t len)
-{
-	if (sscanf(buf, "%d\n", &vibrator_level) == 1) {
-		if (vibrator_level >= ARRAY_SIZE(vibrator_duty_levels))
-			vibrator_level = ARRAY_SIZE(vibrator_duty_levels) - 1;
-		vibrator_duty = vibrator_level < 0 ? -1 : vibrator_duty_levels[vibrator_level];
-	}
-	return len;
-}
-
-static DEVICE_ATTR(vibrator_level_max, S_IRUGO | S_IWUGO, show_vibrator_level_max, NULL);
-static DEVICE_ATTR(vibrator_level, S_IRUGO | S_IWUGO, show_vibrator_level, store_vibrator_level);
-
-static int create_vibrator_sysfs(void)
-{
-	int ret;
-	struct kobject *vibrator_kobj;
-	vibrator_kobj = kobject_create_and_add("vibrator", NULL);
-	if (unlikely(!vibrator_kobj))
-		return -ENOMEM;
-
-	ret = sysfs_create_file(vibrator_kobj,
-			&dev_attr_pwm_val.attr);
-	if (unlikely(ret < 0))
-		pr_err("[VIB] sysfs_create_file failed: %d\n", ret);
-
-	ret = sysfs_create_file(vibrator_kobj, &dev_attr_vibrator_level_max.attr);
-	if (unlikely(ret < 0))
-		pr_err("[VIB] sysfs_create_file failed: %d\n", ret);
-
-	ret = sysfs_create_file(vibrator_kobj, &dev_attr_vibrator_level.attr);
-	if (unlikely(ret < 0))
-		pr_err("[VIB] sysfs_create_file failed: %d\n", ret);
-
-	return 0;
-}
-
-static int __devinit vibrator_probe(struct platform_device *pdev)
+static int vibrator_probe(struct platform_device *pdev)
 {
 	struct max8997_dev *max8997 = dev_get_drvdata(pdev->dev.parent);
 	struct max8997_platform_data *max8997_pdata
@@ -327,8 +326,17 @@ static int __devinit vibrator_probe(struct platform_device *pdev)
 		goto err_timed_output_register;
 	}
 
+	/* User controllable pwm level */
+	error = device_create_file(ddata->dev.dev, &dev_attr_pwm_value);
+	if (error < 0) {
+		pr_err("[VIB] create sysfs fail: pwm_value\n");
+	}
+
 #ifdef CONFIG_VIBETONZ
 	g_data = ddata;
+	pwm_duty_max = g_data->pdata->duty;
+	pwm_duty_min = pwm_duty_max/2;
+	pwm_duty = (pwm_value * pwm_duty_min) / 100 + pwm_duty_min;
 #endif
 
 	return 0;
@@ -344,7 +352,7 @@ err_free_mem:
 	return error;
 }
 
-static int __devexit vibrator_remove(struct platform_device *pdev)
+static int vibrator_remove(struct platform_device *pdev)
 {
 	struct vibrator_drvdata *data = platform_get_drvdata(pdev);
 	timed_output_dev_unregister(&data->dev);
@@ -372,7 +380,7 @@ static int vibrator_resume(struct platform_device *pdev)
 
 static struct platform_driver vibrator_driver = {
 	.probe	= vibrator_probe,
-	.remove	= __devexit_p(vibrator_remove),
+	.remove	= vibrator_remove,
 	.suspend = vibrator_suspend,
 	.resume	= vibrator_resume,
 	.driver	= {
